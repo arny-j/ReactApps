7ab1a5a50a326271ce37a5d91b0940c1db966384
Jack Arnold <cygnusofbalance@gmail.com>
2025-03-13 14:20:25 +1300

    Changed the Menu items code

--- JS-ReactApp/src/Menu.jsx
+++ JS-ReactApp/src/Menu.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable react/prop-types */
 function Menu({ monsters, onSelect }) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary text-white">
-      <div className="container-fluid">
-        <ul className="navbar-nav p-3">
-          {monsters.map((monster, index) => (
+    // <nav className="navbar navbar-expand-lg bg-body-tertiary text-white">
+    //   <div className="container-fluid">
+    //     <ul className="navbar-nav p-3">
+    //       {monsters.map((monster, index) => (
+    //         <button
+    //           key={index}
+    //           onClick={() => onSelect(monster)}
+    //           className="nav-item m-2 p-3 btn btn-secondary text-white shadow">{monster.name}</button>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </nav>
+
+    <nav className="navbar bg-body-tertiary">
+        <div className="container">
+        {monsters.map((monster, index) => (
             <button
               key={index}
               onClick={() => onSelect(monster)}
               className="nav-item m-2 p-3 btn btn-secondary text-white shadow">{monster.name}</button>
           ))}
-        </ul>
-      </div>
+        </div>
     </nav>
   );
 }
